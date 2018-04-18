ba7b5c811e2b60219baf77ba60688fa26b6938bb
Shidairyu <shidairyu@gmail.com>
2018-04-18 11:34:47 +0200

    Upload program

---
+++ GBCalendar/GBCalendar/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace GBCalendar
+{
+	public partial class App : Application
+	{
+        public static bool IsUserLoggedIn { get; set; }
+
+		public App ()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+            MainPage = new NavigationPage(new StartPage());
+        }
+
+		protected override void OnSleep ()
+		{
+            // Handle when your app sleeps
+            System.Threading.Thread.Sleep(300000);
+            IsUserLoggedIn = false;
+        }
+
+		protected override void OnResume ()
+		{
+            // Handle when your app resumes
+            //Sets the right starter page
+            if (!IsUserLoggedIn)
+            {
+                MainPage = new NavigationPage(new StartPage());
+            }
+            else
+            {
+                MainPage = new NavigationPage(new MainPage());
+            }
+        }
+	}
+}
---
+++ GBCalendar/GBCalendar/StartPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GBCalendar
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StartPage : ContentPage
+	{
+        private Int16 IdRole { get; set; }
+
+        public StartPage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void OnStudentClicked(object sender, EventArgs args)
+        {
+            IdRole = 2;
+            await Navigation.PushAsync(new LoginPage(IdRole));
+        }
+
+        async void OnTeacherClicked(object sender, EventArgs args)
+        {
+            IdRole = 1;
+            await Navigation.PushAsync(new LoginPage(IdRole));
+        }
+    }
+}
